fad3a194c7a0cfcc7255a72f56e5f4774f778630
DevKeerthika <keerthika.balu.cs@gmail.com>
2024-05-29 16:26:43 +0530

    def merged with master

--- src/com/jsp/git/App.java
+++ src/com/jsp/git/App.java
@@ -1,17 +1,11 @@
 package com.jsp.git;
 
-<<<<<<< HEAD
 public class App 
 {
 	//Add feature 1
 	
 	// I am  master
-=======
-public class App {
 	
-	// added feature abc
-	
-	//I am abc branch
+	// Added feature 2
 
->>>>>>> abc
 }
